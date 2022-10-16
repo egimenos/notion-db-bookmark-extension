4f9a5a08a1ee483408549e2e9332b248eb422a5b
EgimenoS <ergisan@gmail.com>
2022-10-16 17:57:59 +0200

    edit styles for alert

--- next-app/components/Alert.js
+++ next-app/components/Alert.js
@@ -6,7 +6,7 @@ const Alert = ({ title, message, type = "info" }) => {
   const dynamicStyles = {
     success: {
       "bg-100": "bg-green-100",
-      "border-400": "border-green-400",
+      "border-400": "border-green-800",
       "text-700": "text-green-700",
       "text-500": "text-green-500",
     },
@@ -31,19 +31,20 @@ const Alert = ({ title, message, type = "info" }) => {
 
   return (
     <div
-      className={`${dynamicStyles[type]["bg-100"]} border border-${
+      className={`${dynamicStyles[type]["bg-100"]} border text-lg ${
         dynamicStyles[type]["border-400"]
-      } text-${dynamicStyles[type]["text-700"]} px-4 py-3 rounded relative ${
+      } ${dynamicStyles[type]["text-700"]} px-2 py-1 rounded relative ${
         isShow || "hidden"
       }`}
       role="alert"
     >
       <strong className="font-bold">{title}</strong>
-      <span className="block sm:inline">{message}</span>
-      <button onClick={handleClose}>
-        <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+      <span className="text-md block sm:inline">{message}</span>
+
+      <span className="absolute top-0 bottom-0 right-0 px-2 py-1">
+        <button onClick={handleClose}>
           <svg
-            className={`fill-current h-6 w-6 text-${dynamicStyles[type]["text-500"]}`}
+            className={`fill-current h-6 w-6 ${dynamicStyles[type]["text-500"]}`}
             role="button"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
@@ -51,8 +52,8 @@ const Alert = ({ title, message, type = "info" }) => {
             <title>Close</title>
             <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
           </svg>
-        </span>
-      </button>
+        </button>
+      </span>
     </div>
   );
 };
